b644e9145bc539e55ed6fc516433f127c021ea8b
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-05-23 15:02:09 -0400

    fixed variable names

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,13 +8,11 @@ import os
 import csv
 
 # declaring top level variables
-poll_csv = "./Resources/election_data.csv"
-results_file = "./analysis/results.txt"
-results_dict = dict()
-totalVote, votePercentage = 0, 0
+pollCSV, resultsFile = "./Resources/election_data.csv", "./analysis/results.txt"
+resultsDictonary, totalVote, votePercentage = dict(), 0, 0
 
 # opening the csv and creating reader object
-with open(poll_csv, encoding='utf-8') as csvfile:
+with open(pollCSV, encoding='utf-8') as csvfile:
     poll_reader = csv.reader(csvfile)
     print("Processing Please Wait ")
     # skipping header and then looping through csv
@@ -23,26 +21,26 @@ with open(poll_csv, encoding='utf-8') as csvfile:
         candidate = row[2]
         totalVote += 1
         # creating/updating entry in dictonary
-        if candidate in results_dict:
-            results_dict[candidate] += 1
+        if candidate in resultsDictonary:
+            resultsDictonary[candidate] += 1
         else:
-            results_dict.update({candidate: 1})   
+            resultsDictonary.update({candidate: 1})   
 
 # clearing screen, opening analysis file and displaying & writing results through user defined function
 os.system('cls' if os.name == 'nt' else 'clear')
-outputFile = open(results_file, "w")
+outputFile = open(resultsFile, "w")
 
 outputResults(outputFile, "Election Results")
 outputResults(outputFile, "-------------------------")
 outputResults(outputFile, "Total Votes: " + str(totalVote))
 outputResults(outputFile, "-------------------------")
 
-for key in results_dict:
-    votePercentage = round((results_dict[key] / totalVote) * 100, 3)
-    outputResults(outputFile, key + ":  " + str(votePercentage) + "% (" + str(results_dict[key]) +")")
+for key in resultsDictonary:
+    votePercentage = round((resultsDictonary[key] / totalVote) * 100, 3)
+    outputResults(outputFile, key + ":  " + str(votePercentage) + "% (" + str(resultsDictonary[key]) +")")
 
 outputResults(outputFile, "-------------------------")
-outputResults(outputFile, "Winner: " + max(results_dict, key=results_dict.get))
+outputResults(outputFile, "Winner: " + max(resultsDictonary, key=resultsDictonary.get))
 outputResults(outputFile, "-------------------------")
 
 # closing results file
